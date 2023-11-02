28006ae2fedaf1ecb4c8e6c07d43cf05fda5289e
Krystian Górski a.k.a HamsterIT <128668800+TerooHamster@users.noreply.github.com>
2023-11-02 20:40:09 +0100

    Add files via upload

---
+++ 6b.routing/app/transfer/CalcResult.class.php
@@ -0,0 +1,16 @@
+<?php namespace app\transfer;
+class CalcResult {
+	public $rodzaj;
+	public $oprocentowanie;
+	public $odsetki;
+	public $koszt;
+	public $miesiac; 
+	public $calkowita;	
+	public $parametr1;
+	public $parametr2;
+	public $parametr3;
+	public $parametr4;
+	public $parametr5;
+	public $parametr6;	
+}
+?>
